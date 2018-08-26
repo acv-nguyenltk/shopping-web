0e6f02f8b239f1a920cb3c4a1a19e2f55371aca7
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-26 14:45:46 +0700

    add Devise Trackable and 1st,last name to users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-        sessions: 'users/sessions' }
   root 'products#index'
+  devise_for :users, controllers: {
+        sessions: 'users/sessions', registrations: 'users/registrations' }
   resource :products
 end
