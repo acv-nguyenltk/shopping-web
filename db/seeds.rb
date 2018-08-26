9e5178a1c4a016ac892fa5b5781b191b43a60805
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-26 14:36:30 +0700

    add users routes, add destroy data seeds data, migration table users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 require 'faker'
 
+Product.destroy_all
+Category.destroy_all
+
+tables = ['products', 'categories','users']
+start_id = 1
+
+tables.each do |table|
+   ActiveRecord::Base.connection.execute(
+     "ALTER SEQUENCE #{table}_id_seq RESTART WITH #{start_id}"
+   )
+end
+
 2.times do
   category = Category.create(name: Faker::FunnyName.name)
   20.times do
