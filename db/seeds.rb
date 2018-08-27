33f60ea875b1c0fac86af2b8bb3c8e086e085490
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-27 13:22:50 +0700

    add devise gem, create login logout and allow user can update info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
 require 'faker'
 
+Product.destroy_all
+Category.destroy_all
+User.destroy_all
+
+tables = ['products', 'categories', 'users']
+start_id = 1
+
+tables.each do |table|
+   ActiveRecord::Base.connection.execute(
+     "ALTER SEQUENCE #{table}_id_seq RESTART WITH #{start_id}"
+   )
+end
+
+User.create(
+  email: 'knguyen30111@gmail.com',
+  firstname: 'nguyen',
+  lastname: 'le',
+  password: '123456',
+  phoneNumber: '12345678',
+  address: 'ascb'
+)
+
 2.times do
   category = Category.create(name: Faker::FunnyName.name)
   20.times do
