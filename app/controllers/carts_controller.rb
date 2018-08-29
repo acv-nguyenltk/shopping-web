bd98f2b9b9ea1abaa96a81d0fa9164e97fa4e503
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-29 21:02:19 +0700

    create some method in cartscontroller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,15 +1,29 @@
 class CartsController < ApplicationController
-  before_action :check_session
+  before_action :get_cart
   def index
-
+    @products = []
+    session[:carts].each do |key, val|
+      @products << Product.find(key)
+    end
   end
 
   def change_quatity
-
+    binding.pry
+    if session[:carts].keys.include?(@product.id.to_s)
+      if params[:quatity].to_i <= 0
+        session[:carts].delete(@product.id.to_s)
+      else
+        session[:carts][@product.id.to_s] = params[:quatity].to_i
+      end
+    end
   end
 
   def add
-
+    if session[:carts].keys.include?(@product.id.to_s)
+      session[:carts][@product.id.to_s] += 1
+    else
+      session[:carts][@product.id.to_s] = 1
+    end
   end
 
   def delete
@@ -17,6 +31,12 @@ class CartsController < ApplicationController
   end
 
   def empty
-
+    session[:carts].clear
+    render :index
   end
+  private
+    def get_cart
+      return if params[:id].blank?
+      @product = Product.find(params[:id].to_s)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Custom cart/session cart
   get 'carts' => 'carts#index', :as => 'cart_index'
-  post 'carts' => 'carts#change_quatity', as: 'cart_change'
+  post 'carts/change/:id' => 'carts#change_quatity', as: 'cart_change'
   post 'carts/add/:id' => 'carts#add', :as => 'cart_add'
   delete 'carts/remove/:id' => 'carts#delete', :as => 'cart_delete'
   delete 'carts/empty' => 'carts#empty', :as => 'empty_cart'
