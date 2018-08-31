0d9735adfeec0b9ca6fba15633f9cce225885b6c
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-31 15:46:41 +0700

    delete binding.pry

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,6 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
   def after_sign_in_path_for(resource)
-    binding.pry
     session[:carts] ||= {}
     session[:carts].merge!(current_user.cart.items) { |key, oldval, newval| oldval + newval } if current_user.cart.items.present?
     current_user.cart.items = session[:carts]
