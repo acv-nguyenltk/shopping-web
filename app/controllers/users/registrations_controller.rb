c01ddbb551f4277ede3b5c908bf76394172b2ef3
Nguyen <nguyenltk@atmarkcafe.org>
2018-09-06 18:31:48 +0700

    add address attr in user registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :phoneNumber, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :phoneNumber, addresses_attributes: [:id, :line1, :line2, :city, :country_code, :state, :_destroy]])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phoneNumber, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phoneNumber, addresses_attributes: [:id, :line1, :line2, :city, :country_code, :state, :_destroy]])
   end
 end
