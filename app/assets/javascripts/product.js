99044fb3ab5f456d390b0e9658cdd3c097c21490
Nguyen <nguyenltk@atmarkcafe.org>
2018-09-17 14:14:47 +0700

    can update product's info (ajax)

---
+++ app/assets/javascripts/product.js
@@ -0,0 +1,64 @@
+function showDetailProduct(id){
+  req = $.ajax({
+    url: '/products/' + id,
+  });
+  req.done(function(data){
+    $('#cart').modal('hide');
+    htmlChange(data);
+    $('#showItem .modal-body').html(htmlChange(data));
+    $('#showItem').modal('show');
+  });
+  req.fail(function(data){
+    alert('Show failed');
+  });
+};
+
+function updateProduct(id){
+  var name = $('#name').val();
+  var price = $('#price').val();
+  var description = $('#description').val();
+  var data = { name: name, price: price, description: description };
+  req = $.ajax({
+    url: '/products/' + id,
+    data: { product: data },
+    type: 'PUT'
+  });
+
+  req.done(function(data){
+    alert('dsadsa');
+
+  });
+  req.fail(function(data){
+    debugger;
+    alert('something wrong')
+  })
+}
+
+function htmlChange(data){
+  var html = '<div id="showItem">' +
+            '<div class="row">' +
+              '<div>' +
+                '<div class="float-left">' +
+                  '<img src="'+ data.imgUrl +'" height="50%" width="50%"> ' +
+                '</div>'+
+                '<div class="inline">' +
+                  '<div>'+ 'Name : ' +
+                    '<input id="name" type="text" value="' + data.name + '">' +
+                  '</div>'+
+                  '<div>'+ 'Price : ' +
+                    '<input id="price" type="text" value="' + data.price + '">' +
+                  '</div>'+
+                  '<div>'+ 'Description : ' +
+                    '<textarea id="description">' + data.description + '</textarea>' +
+                  '</div>'+
+                '</div>'+
+              '</div>' +
+              '<br>' +
+              '<div>'+
+                '<button type="button" class="btn btn-primary update_product" onclick="updateProduct('+ data.id +')">Update</button>' +
+              '</div>'+
+            '</div>' +
+          '</div>' +
+          '<div class="clearfix"></div>';
+  return html;
+}
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,21 @@
 class ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @products = (Product.page params[:page]).per(12)
   end
 
   def show
     @product = Product.find(params[:id])
+    render json: @product
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    render json: @product
+  end
+  private def product_params
+    params.require(:product).permit( :name, :description, :price)
   end
 end
